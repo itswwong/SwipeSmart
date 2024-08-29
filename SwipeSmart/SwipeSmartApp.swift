5fb48c33822ba302343704ccc81015673f57263c
William Wong <wwong10th@gmail.com>
2024-08-28 17:36:31 -0700

    Initial Commit

---
+++ SwipeSmart/SwipeSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeSmartApp.swift
+//  SwipeSmart
+//
+//  Created by William Wong on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
