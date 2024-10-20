aec575c32b752171e2656571de4b0b4f8fab193d
Tyler Xiao <yxiao1717@gmail.com>
2024-10-19 19:00:58 -0700

    create widget, integrate data with widget

---
+++ SwipeSmartWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  SwipeSmartWidget
+//
+//  Created by Tyler Xiao on 10/18/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
