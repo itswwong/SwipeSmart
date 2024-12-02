6d52ae66693ac6bf9c82834299096558c617035d
Lindsay Qin <lindsaymqin@gmail.com>
2024-12-02 13:09:29 -0800

    Merge remote-tracking branch 'origin/main' into lq-rewardsview

--- SwipeSmart/Views/Cards/CardView.swift
+++ SwipeSmart/Views/Cards/CardView.swift
@@ -1,3 +1,10 @@
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(card: CreditCard.sampleCards[0])
+    }
+}
+
 //
 //  CardView.swift
 //
@@ -12,30 +19,32 @@ struct CardView: View {
     var body: some View {
         VStack(alignment: .leading) {
             HStack {
-                Text(card.bankName)
-                    .font(.custom("Inter-Regular_Thin", size: 15))
-                    .padding(.bottom, 2)
-                    .foregroundStyle(.pastelgraylight)
-                Spacer()
-            }
-            Spacer()
-            HStack {
-                Spacer()
-                if !card.cardName.isEmpty {
+                VStack(alignment: .leading){
+                    Text(card.bankName)
+                        .font(.title3.weight(.bold))
+                        .padding(.bottom, 5)
                     Text(card.cardName)
-                        .font(.custom("Inter-Regular_Light", size: 15))
-                        .padding(.bottom, 15)
+                        .multilineTextAlignment(.trailing)
+                    Spacer()
                 }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text((card.cardType == "Discover" || card.cardType == "American Express") ? "" : card.cardType)
+                        .font(.title3.weight(.bold))
+                    Spacer()
+                }
+                .padding(.trailing, -25)
             }
-            Spacer()
+            RoundedRectangle(cornerRadius: 7)
+                .frame(width: 30, height: 20)
+                .padding(.bottom, 5)
+                .foregroundStyle(Color("pastelgraylight"))
             HStack {
                 if card.digits.isEmpty {
                     Text("**** **** **** ****")
-                        .font(.custom("Inter-Regular_Light", size: 15))
                 }
                 else {
                     Text("**** **** **** \(card.digits)")
-                        .font(.custom("Inter-Regular_Light", size: 15))
                 }
             }
             Spacer()
@@ -65,13 +74,6 @@ struct CardView: View {
                 }
             }
         }
-        .padding(.leading, 20)
-        .frame(height: 170)
-    }
-}
-
-struct CardView_Previews: PreviewProvider {
-    static var previews: some View {
-        CardView(card: CreditCard.sampleCards[0])
+        .padding()
     }
 }
