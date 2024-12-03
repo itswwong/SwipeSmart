822a88808021f304c57ad643cab104f669e9c3cb
emilyzhang625 <139720917+emilyzhang625@users.noreply.github.com>
2024-12-03 15:05:53 -0800

    got cancel and done buttons back, logic behind buttons should work now

--- SwipeSmart/Views/Cards/DetailEditView.swift
+++ SwipeSmart/Views/Cards/DetailEditView.swift
@@ -27,106 +27,132 @@ struct DetailEditView: View {
 
     var body: some View {
         NavigationStack {
-        VStack {
-            HStack {
-                Text("BANK NAME")
-                    .foregroundColor(Color.gray)
-                Spacer()
-                TextField("", text: $card.bankName)
-                    .padding(10)
-                    .onChange(of: card.bankName) {
-                        validateUniqueDigits()
+            VStack {
+                HStack {
+                    Text("BANK NAME")
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    TextField("", text: $card.bankName)
+                        .padding(10)
+                        .onChange(of: card.bankName) {
+                            validateUniqueDigits()
+                        }
+                        .overlay(RoundedRectangle(cornerRadius:5).stroke(Color.gray, lineWidth: 1))
+                        .foregroundColor(customMidGray)
+                        .frame(width: 200)
+                }
+                .padding(.vertical, 10)
+                HStack {
+                    Text("CARD TYPE")
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    Menu {
+                        Picker(selection: $card.cardType, label: Text("")) {
+                            ForEach(cardTypeList, id: \.self) { type in
+                                Text(type).tag(type)
+                            }
+                        }
                     }
+                    label: {
+                        HStack {
+                            Text(card.cardType.isEmpty ? "Select Card Type" : card.cardType)
+                                .foregroundStyle(Color.gray)
+                                .padding(10)
+                            Image(systemName: "chevron.down")
+                                .foregroundStyle(Color.gray)
+                        }
+                        .frame(width: 200, height: 45)
+                        .overlay(RoundedRectangle(cornerRadius:5).stroke(customLightGray, lineWidth: 1))
+                        .background(customLightGray)
+                    }
+                }
+                .padding(.vertical, 10)
+                HStack {
+                    Text("CARD NAME")
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    TextField(text: $card.cardName, label: {
+                        Text("Optional")
+                    })
+                    .padding(10)
                     .overlay(RoundedRectangle(cornerRadius:5).stroke(Color.gray, lineWidth: 1))
                     .foregroundColor(customMidGray)
                     .frame(width: 200)
-            }
-            .padding(.vertical, 10)
-            HStack {
-                Text("CARD TYPE")
-                    .foregroundColor(Color.gray)
-                Spacer()
-                Menu {
-                    Picker(selection: $card.cardType, label: Text("")) {
-                        ForEach(cardTypeList, id: \.self) { type in
-                            Text(type).tag(type)
+                }
+                .padding(.vertical, 10)
+                HStack {
+                    Text("LAST 4 DIGITS")
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    TextField(text: $card.digits, label: {
+                        Text("Optional")
+                    })
+                    .foregroundColor(customMidGray)
+                    
+                    .keyboardType(.numberPad)
+                    .onReceive(Just($card.digits)) { _ in
+                        limitText()
+                        validateUniqueDigits()
+                    }
+                    .foregroundStyle(duplicateError ? .red : .primary)
+                    .padding(10)
+                    .overlay(RoundedRectangle(cornerRadius:5).stroke(Color.gray, lineWidth: 1))
+                    .frame(width: 200)
+                }
+                .padding(.vertical, 10)
+                HStack {
+                    Text("BACKGROUND")
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    ZStack {
+                        ColorPicker(selection: $card.theme)
+                        HStack {
+                            Text("Color")
+                                .foregroundStyle(Color.gray)
+                                .padding(10)
+                            Image(systemName: "chevron.right")
+                                .foregroundStyle(Color.gray)
                         }
                     }
+                    .frame(width: 217, height: 55)
+                    .padding(.trailing, -4)
                 }
-                label: {
-                    HStack {
-                        Text(card.cardType.isEmpty ? "Select Card Type" : card.cardType)
-                            .foregroundStyle(Color.gray)
-                            .padding(10)
-                        Image(systemName: "chevron.down")
-                            .foregroundStyle(Color.gray)
-                    }
-                    .frame(width: 200, height: 45)
-                    .overlay(RoundedRectangle(cornerRadius:5).stroke(customLightGray, lineWidth: 1))
-                    .background(customLightGray)
-                }
-            }
-            .padding(.vertical, 10)
-            HStack {
-                Text("CARD NAME")
-                    .foregroundColor(Color.gray)
-                Spacer()
-                TextField(text: $card.cardName, label: {
-                    Text("Optional")
-                })
-                .padding(10)
-                .overlay(RoundedRectangle(cornerRadius:5).stroke(Color.gray, lineWidth: 1))
-                .foregroundColor(customMidGray)
-                .frame(width: 200)
-            }
-            .padding(.vertical, 10)
-            HStack {
-                Text("LAST 4 DIGITS")
-                    .foregroundColor(Color.gray)
-                Spacer()
-                TextField(text: $card.digits, label: {
-                    Text("Optional")
-                })
-                .foregroundColor(customMidGray)
+                .padding(.bottom, 20)
                 
-                .keyboardType(.numberPad)
-                .onReceive(Just($card.digits)) { _ in
-                    limitText()
-                    validateUniqueDigits()
+                if duplicateError {
+                    Text("Credit card already exists.")
+                        .foregroundStyle(.red)
+                        .font(.footnote)
                 }
-                .foregroundStyle(duplicateError ? .red : .primary)
-                .padding(10)
-                .overlay(RoundedRectangle(cornerRadius:5).stroke(Color.gray, lineWidth: 1))
-                .frame(width: 200)
             }
-            .padding(.vertical, 10)
-            HStack {
-                Text("BACKGROUND")
-                    .foregroundColor(Color.gray)
-                Spacer()
-                ZStack {
-                    ColorPicker(selection: $card.theme)
-                    HStack {
-                        Text("Color")
-                            .foregroundStyle(Color.gray)
-                            .padding(10)
-                        Image(systemName: "chevron.right")
-                            .foregroundStyle(Color.gray)
+            .padding()
+            .toolbar {
+                // Add Cancel button
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        presentationMode.wrappedValue.dismiss()
+//                        isPresentingEditView = false
                     }
+                    .tint(.blue)
                 }
-                .frame(width: 217, height: 55)
-                .padding(.trailing, -4)
+                
+                // Add Done button
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        presentationMode.wrappedValue.dismiss()
+//                        isPresentingEditView = false
+                        duplicateError = false
+                        categories = categories
+                        card = card
+                        // Perform additional save or validation logic here
+                    }
+                    .disabled(card.bankName.isEmpty || card.cardType.isEmpty || duplicateError)
+                    .tint(card.bankName.isEmpty || card.cardType.isEmpty || duplicateError ? .gray : .blue)
+                }
             }
-            .padding(.bottom, 20)
-            
-            if duplicateError {
-                Text("Credit card already exists.")
-                    .foregroundStyle(.red)
-                    .font(.footnote)
-            }
-        }.padding() //end of vstack
-
-    }                                /*.toolbar {
+        }
+    
+        /*.toolbar {
                                             ToolbarItem(placement: ToolbarItemPlacement.navigationBarLeading) {
                                                 Button("Cancel") {
                                                   isPresentingEditView = false
@@ -237,3 +263,4 @@ struct DetailEditView_Previews: PreviewProvider {
         DetailEditView(card: .constant(CreditCard.sampleCards[0]), cards: .constant(CreditCard.sampleCards), categories: .constant(Category.sampleCategories), duplicateError: .constant(false), showDelete: true, onDeleteCard: {})
     }
 }
+
